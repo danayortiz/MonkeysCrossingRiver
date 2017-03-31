b62a500124eee059a9ed01458d7cdbbb8a2e83d2
Danay Ortiz <Danay Ortiz>
2017-03-30 20:27:36 -0600

    Fixes tests broken when `output` argument added

--- MonkeysCrossingRiverTests/MonkeyManagerTest.cs
+++ MonkeysCrossingRiverTests/MonkeyManagerTest.cs
@@ -7,38 +7,40 @@ namespace MonkeysCrossingRiverTests
     [TestClass]
     public class MonkeyManagerTest
     {
+        public IOutput output = new TestOutput();
+
         [TestMethod]
         public void TestRun_PassingNoMonkeys_ShouldReturnCeroIterations()
         {
-            var monkeyManager = new MonkeyManager(0, 0);
+            var monkeyManager = new MonkeyManager(output, 0, 0);
             Assert.AreEqual(0, monkeyManager.Run());
         }
 
         [TestMethod]
         public void TestRun_PassingOneMonkeyLeftSide_ShouldReturnFourIterations()
         {
-            var monkeyManager = new MonkeyManager(1, 0);
+            var monkeyManager = new MonkeyManager(output, 1, 0);
             Assert.AreEqual(4, monkeyManager.Run());
         }
 
         [TestMethod]
         public void TestRun_PassingOneMonkeyRightSide_ShouldReturnFourIterations()
         {
-            var monkeyManager = new MonkeyManager(0, 1);
+            var monkeyManager = new MonkeyManager(output, 0, 1);
             Assert.AreEqual(4, monkeyManager.Run());
         }
 
         [TestMethod]
         public void TestRun_PassingOneMonkeyEachSide_ShouldReturnEightIterations()
         {
-            var monkeyManager = new MonkeyManager(1, 1);
+            var monkeyManager = new MonkeyManager(output, 1, 1);
             Assert.AreEqual(8, monkeyManager.Run());
         }
 
         [TestMethod]
         public void TestRun_PassingMultipleMonkeys_ShouldReturnTwentyEightIterations()
         {
-            var monkeyManager = new MonkeyManager(4, 9);
+            var monkeyManager = new MonkeyManager(output, 4, 9);
             Assert.AreEqual(28, monkeyManager.Run());
         }
     }
---
+++ MonkeysCrossingRiverTests/TestOutput.cs
@@ -0,0 +1,32 @@
+﻿using MonkeysCrossingRiverProject.lib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonkeysCrossingRiverTests
+{
+    class TestOutput : IOutput
+    {
+        public void Finish(int iterations)
+        {
+            
+        }
+
+        public void Start(int rightCount, int leftCount)
+        {
+            
+        }
+
+        public void Wait()
+        {
+            
+        }
+
+        public void Write(MonkeyManager.Sides side, int count, bool[] ropePositions)
+        {
+            
+        }
+    }
+}
